28f61ae0f77ad90e5609acc141af3c9c7fb912b9
Leina33 <dominickrutto@gmail.com>
2020-01-19 14:01:21 +0300

    create file name forms.py which will have classes form form

--- hoods/models.py
+++ hoods/models.py
@@ -105,3 +105,214 @@ class Neighbourhood(models.Model):
     neighbourhood_name = models.CharField(max_length=30)
     neighbourhood_location = models.CharField(choices=CITY_CHOICES, max_length=200 ,default=0, null=True, blank=True)
     population= models.IntegerField(default=0, null=True, blank=True)
+    
+     def __str__(self):
+        return self.neighbourhood_name
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood_by_id(cls, id):
+        neighbourhoods = cls.objects.filter(pk=id)
+        neighbourhoods.delete()
+
+    @classmethod
+    def get_neighbourhood_by_id(cls, id):
+        neighbourhoods = cls.objects.get(pk=id)
+        return neighbourhoods
+
+    @classmethod
+    def filter_by_location(cls, location):
+        neighbourhoods = cls.objects.filter(location=location)
+        return neighbourhoods
+
+    @classmethod
+    def search_neighbourhood(cls, search_term):
+        neighbourhoods = cls.objects.filter(neighbourhood_name__icontains=search_term)
+        return neighbourhoods
+
+    @classmethod
+    def update_neighbourhood(cls, id):
+        neighbourhoods = cls.objects.filter(id=id).update(id=id)
+        return neighbourhoods
+
+    @classmethod
+    def update_neighbourhood(cls, id):
+        neighbourhoods = cls.objects.filter(id=id).update(id=id)
+        return neighbourhoods
+
+class Business(models.Model):
+    business_name = models.CharField(max_length=30, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="business")
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,related_name="neighbourhoodbusiness",null=True,blank=True)
+    business_email_address = models.CharField(max_length=200, null = True)
+
+    def __str__(self):
+        return self.business_name
+
+
+    def save_business(self):
+        self.save()
+
+    @classmethod
+    def delete_business_by_id(cls, id):
+        businesses = cls.objects.filter(pk=id)
+        businesses.delete()
+
+    @classmethod
+    def get_businesses_by_id(cls, id):
+        businesses = cls.objects.get(pk=id)
+        return businesses
+
+    @classmethod
+    def filter_by_location(cls, location):
+        businesses = cls.objects.filter(location=location)
+        return businesses
+
+    @classmethod
+    def search_businesses(cls, search_term):
+        businesses = cls.objects.filter(business_name__icontains=search_term)
+        return businesses
+
+    @classmethod
+    def update_business(cls, id):
+        businesses = cls.objects.filter(id=id).update(id=id)
+        return businesses
+
+    @classmethod
+    def update_business(cls, id):
+        businesses = cls.objects.filter(id=id).update(id=id)
+        return businesses
+
+class Project(models.Model):
+    title = models.TextField(max_length=200, null=True, blank=True, default="title")
+    project_image = models.ImageField(upload_to='picture/', null=True, blank=True)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="neighbourhoodproject",null=True,blank=True)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,related_name="neighbourhoodproject",null=True,blank=True)
+
+    def average_design(self):
+        design_ratings = list(map(lambda x: x.design_rating, self.reviews.all()))
+        return np.mean(design_ratings)
+
+    def average_usability(self):
+        usability_ratings = list(map(lambda x: x.usability_rating, self.reviews.all()))
+        return np.mean(usability_ratings)
+
+    def average_content(self):
+        content_ratings = list(map(lambda x: x.content_rating, self.reviews.all()))
+        return np.mean(content_ratings)
+
+    def save_project(self):
+        self.save()
+
+    @classmethod
+    def delete_project_by_id(cls, id):
+        projects = cls.objects.filter(pk=id)
+        projects.delete()
+
+    @classmethod
+    def get_project_by_id(cls, id):
+        projects = cls.objects.get(pk=id)
+        return projects
+
+    @classmethod
+    def search_projects(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
+
+    @classmethod
+    def update_project(cls, id):
+        projects = cls.objects.filter(id=id).update(id=id)
+        return projects
+
+    @classmethod
+    def update_description(cls, id):
+        projects = cls.objects.filter(id=id).update(id=id)
+        return projects
+
+    def __str__(self):
+        return self.title
+
+class Profile(models.Model):
+    class Meta:
+        db_table = 'profile'
+
+    bio = models.TextField(max_length=200, null=True, blank=True, default="bio")
+    profile_pic = models.ImageField(upload_to='picture/', null=True, blank=True, default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name="profile")
+    project = models.ForeignKey(Project, null=True)
+    email= models.TextField(max_length=200, null=True, blank=True, default=0)
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,related_name="neighbourhood",null=True,blank=True)
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    post_save.connect(create_user_profile, sender=User)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_users(cls, search_term):
+        profiles = cls.objects.filter(user__username__icontains=search_term)
+        return profiles
+
+    @property
+    def image_url(self):
+        if self.profile_pic and hasattr(self.profile_pic, 'url'):
+            return self.profile_pic.url
+
+    def __str__(self):
+        return self.user.username
+
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+
+    )
+    project = models.ForeignKey(Project, null=True, blank=True, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name='reviews')
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="reviews", null=True, blank=True)
+    comment = models.TextField()
+    design_rating = models.IntegerField(choices=RATING_CHOICES, default=0)
+    usability_rating = models.IntegerField(choices=RATING_CHOICES, default=0)
+    content_rating = models.IntegerField(choices=RATING_CHOICES, default=0)
+
+    def save_comment(self):
+        self.save()
+
+    def get_comment(self, id):
+        comments = Review.objects.filter(image_id=id)
+        return comments
+
+    def __str__(self):
+        return self.comment
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Join(models.Model):
+	'''
+	Model that keeps track of what user has joined what neighbourhood
+	'''
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE,related_name="neighbourhooduser",null=True,blank=True)
+	neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,related_name="neighbourhoodjoin",null=True,blank=True)
+
+	def __str__(self):
+		return self.user_id
