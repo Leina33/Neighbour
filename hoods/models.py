c2a3b49af804d7b2169591f01273b5ad0af59391
Leina33 <dominickrutto@gmail.com>
2020-01-19 12:58:54 +0300

    import module np and User which will be use in classes modules

--- hoods/models.py
+++ hoods/models.py
@@ -1,3 +1,7 @@
+from __future__ import unicode_literals
+import numpy as np
+from django.db.models.signals import post_save
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.sql.datastructures import Join
 # Create your models here.
