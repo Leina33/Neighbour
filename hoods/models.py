ebbd06a03b31d10ce9d9385fdfcfb1bdb9457ff3
Leina33 <dominickrutto@gmail.com>
2020-01-19 13:01:07 +0300

    create the class tags with attribute name

--- hoods/models.py
+++ hoods/models.py
@@ -5,3 +5,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.sql.datastructures import Join
 # Create your models here.
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_tags(self):
+        self.save()
+
+    def delete_tags(self):
+        self.delete()
