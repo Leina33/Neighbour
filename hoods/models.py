421409fa48b865ae5e165d374593f6197827e3a0
Leina33 <dominickrutto@gmail.com>
2020-01-19 13:30:47 +0300

    set the neighbourhood field with population,name and location as it's attributes

--- hoods/models.py
+++ hoods/models.py
@@ -101,3 +101,7 @@ class Neighbourhood(models.Model):
         ('Nakuru', 'Nakuru'),
         ('Eldoret', 'Eldoret'),
     )
+    
+    neighbourhood_name = models.CharField(max_length=30)
+    neighbourhood_location = models.CharField(choices=CITY_CHOICES, max_length=200 ,default=0, null=True, blank=True)
+    population= models.IntegerField(default=0, null=True, blank=True)
