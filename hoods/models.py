e3f217d17e96a00a216e6bdd49a16e70caef708e
Leina33 <dominickrutto@gmail.com>
2020-01-20 00:04:31 +0300

    import EmailMultiAlternatives class from Djnago mail module,this will be responsible for sending mail

--- hoods/models.py
+++ hoods/models.py
@@ -40,7 +40,7 @@ class Image(models.Model):
     likes = models.IntegerField(default=0)
     comments = models.TextField(blank=True)
 
-    class Image(models.Model):
+class Image(models.Model):
     image = models.ImageField(upload_to='picture/', )
     name = models.CharField(max_length=40)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="images")
@@ -106,7 +106,7 @@ class Neighbourhood(models.Model):
     neighbourhood_location = models.CharField(choices=CITY_CHOICES, max_length=200 ,default=0, null=True, blank=True)
     population= models.IntegerField(default=0, null=True, blank=True)
     
-     def __str__(self):
+    def __str__(self):
         return self.neighbourhood_name
 
     def save_neighbourhood(self):
